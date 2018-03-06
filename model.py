d56073994ee3e4c79f44b00bd861baa82dcd4fc4
yihao31 <yihaohit@gmail.com>
2018-03-06 10:25:59 +0800

    no comment

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch 
+import numpy as np 
+import sys 
+#使用pre-trained的alexnet
+
+class Model:
+    def alexnet_visible():
+
+    def alexnet_thermal():
+
+    def share_modal(feature1,feature2,_dropout):
